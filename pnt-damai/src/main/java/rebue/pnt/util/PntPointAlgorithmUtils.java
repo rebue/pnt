14977cad7256003ce3499b13b4edcd29f7dcb34c
lbl <lbl@lbl>
2018-12-24 17:31:19 +0800

    补充修改points改为point

--- pnt-damai/src/main/java/rebue/pnt/util/PntPointAlgorithmUtils.java
+++ pnt-damai/src/main/java/rebue/pnt/util/PntPointAlgorithmUtils.java
@@ -10,9 +10,9 @@ import org.slf4j.LoggerFactory;
  * @author lbl
  * 2018年12月21日10:07:37
  */
-public class PntPointsAlgorithmUtils {
+public class PntPointAlgorithmUtils {
 
-	private static final Logger _log = LoggerFactory.getLogger(PntPointsAlgorithmUtils.class);
+	private static final Logger _log = LoggerFactory.getLogger(PntPointAlgorithmUtils.class);
 	
 	/**
 	 * 返佣积分算法（非首单） = 销售价 / 2 - 成本价
@@ -20,11 +20,11 @@ public class PntPointsAlgorithmUtils {
 	 * @param costPrice 成本价
 	 * @return
 	 */
-	public static BigDecimal commissionPoints(BigDecimal salePrice, BigDecimal costPrice) {
+	public static BigDecimal commissionPoint(BigDecimal salePrice, BigDecimal costPrice) {
 		_log.info("计算积分的参数为：salePrice={}, costPrice={}", salePrice, costPrice);
-		BigDecimal points = salePrice.divide(BigDecimal.valueOf(2)).subtract(costPrice);
-		_log.info("计算积分的返回值为：{}", points);
-		return points;
+		BigDecimal point = salePrice.divide(BigDecimal.valueOf(2)).subtract(costPrice);
+		_log.info("计算积分的返回值为：{}", point);
+		return point;
 	}
 	
 	/**
@@ -32,10 +32,10 @@ public class PntPointsAlgorithmUtils {
 	 * @param salePrice 销售价
 	 * @return
 	 */
-	public static BigDecimal firstOrderPoints(BigDecimal salePrice) {
+	public static BigDecimal firstOrderPoint(BigDecimal salePrice) {
 		_log.info("计算首单积分的参数为：salePrice={}", salePrice);
-		BigDecimal points = salePrice.multiply(BigDecimal.valueOf(0.5));
-		_log.info("计算首单积分的返回值为：{}", points);
-		return points;
+		BigDecimal point = salePrice.multiply(BigDecimal.valueOf(0.5));
+		_log.info("计算首单积分的返回值为：{}", point);
+		return point;
 	}
 }
