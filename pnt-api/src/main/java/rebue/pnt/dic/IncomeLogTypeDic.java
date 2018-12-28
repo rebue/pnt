f5eb7e2683b8e8ed3fb25aaab8e46935ee14db3a
zbz <nnzbz@qq.com>
2018-12-28 22:15:05 +0800

    * 修正字典类 * 修正模板

--- pnt-api/src/main/java/rebue/pnt/dic/IncomeLogTypeDic.java
+++ pnt-api/src/main/java/rebue/pnt/dic/IncomeLogTypeDic.java
@@ -9,60 +9,67 @@ import rebue.wheel.baseintf.EnumBase;
 
 /**
  * 收益日志类型字典
+ * 
  * @author lbl
- * 2018年12月20日10:17:39
+ *         2018年12月20日10:17:39
  */
 public enum IncomeLogTypeDic implements EnumBase {
 
-	/**
-	 * 日收益（收益+）
-	 */
-	DAY_INCOME(1),
-	
-	/**
-	 * 转出收益（收益-）
-	 */
-	TRANSFER_OUT_INCOME(2);
-	
-	/**
-	 * 枚举的所有项，注意这个变量是静态单例的
-	 */
-	private static Map<Integer, EnumBase> valueMap;
-	// 初始化map，保存枚举的所有项到map中以方便通过code查找
-	static {
-		valueMap = new HashMap<>();
-		for (EnumBase item : values()) {
-			valueMap.put(item.getCode(), item);
-		}
-	}
+    /**
+     * 日收益（收益+）
+     */
+    DAY_INCOME(1),
 
-	/**
-	 * jackson反序列化时，通过code得到枚举的实例 注意：此方法必须是static的方法，且返回类型必须是本枚举类，而不能是接口EnumBase
-	 * 否则jackson将调用默认的反序列化方法，而不会调用本方法
-	 */
-	@JsonCreator
-	public static IncomeLogTypeDic getItem(int code) {
-		EnumBase result = valueMap.get(code);
-		if (result == null) {
-			throw new IllegalArgumentException("输入的code" + code + "不在枚举的取值范围内");
-		}
-		return (IncomeLogTypeDic) result;
-	}
+    /**
+     * 转出收益（收益-）
+     */
+    TRANSFER_OUT_INCOME(2);
 
-	private int code;
+    /**
+     * 枚举的所有项，注意这个变量是静态单例的
+     */
+    private static Map<Integer, EnumBase> valueMap;
+    // 初始化map，保存枚举的所有项到map中以方便通过code查找
+    static {
+        valueMap = new HashMap<>();
+        for (final EnumBase item : values()) {
+            valueMap.put(item.getCode(), item);
+        }
+    }
 
-	/**
-	 * 构造器，传入code
-	 */
-	IncomeLogTypeDic(int code) {
-		this.code = code;
-	}
+    /**
+     * jackson反序列化时，通过code得到枚举的实例 注意：此方法必须是static的方法，且返回类型必须是本枚举类，而不能是接口EnumBase
+     * 否则jackson将调用默认的反序列化方法，而不会调用本方法
+     */
+    @JsonCreator
+    public static IncomeLogTypeDic getItem(final int code) {
+        final EnumBase result = valueMap.get(code);
+        if (result == null) {
+            throw new IllegalArgumentException("输入的code" + code + "不在枚举的取值范围内");
+        }
+        return (IncomeLogTypeDic) result;
+    }
 
-	/**
-	 * @return jackson序列化时，输出枚举实例的code
-	 */
-	@Override
-	public int getCode() {
-		return code;
-	}
+    private int code;
+
+    /**
+     * 构造器，传入code
+     */
+    IncomeLogTypeDic(final int code) {
+        this.code = code;
+    }
+
+    /**
+     * @return jackson序列化时，输出枚举实例的code
+     */
+    @Override
+    public int getCode() {
+        return code;
+    }
+
+    @Override
+    public String getName() {
+        return name();
+    }
+
 }
